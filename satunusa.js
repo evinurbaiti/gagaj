3fef43c0aad3c9dd28fdc8f157d17c925fdc3ce9
evinurbaiti <nurbaitievi21@gmail.com>
2024-03-07 18:09:17 +0700

    satu nusa

---
+++ satunusa.js
@@ -0,0 +1,12 @@
+function jumlahvolumeDuaKubus (a,b) {
+    var volumeA;
+    var volumeB;
+    var total;
+
+    volumeA = a*a*a;
+    volumeB = b*b*b;
+
+    total = volumeA + volumeB;
+    return total;
+}
+jumlahvolumeDuaKubus(8,3);
